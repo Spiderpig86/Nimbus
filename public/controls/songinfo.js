33a5cd07f3c90c69d388fa81ccbb4ad754488fac
Stan <slim679975@gmail.com>
2017-06-14 09:05:22 -0400

    Better handling of overflow in UI

--- public/controls/songinfo.js
+++ public/controls/songinfo.js
@@ -1,14 +1,14 @@
 const SongInfo = (largeArt, track, tags, errorPurchase) => (
     `
         <div class="card level">
-            <div class="content text-center">
-                <div class="content text-center">
-                    <div class="row">
+            <div class="content-no-padding text-center">
+                <div class="content-no-padding text-center">
+                    <div class="row center">
                         <div class="col-12">
                             <div class="flipContainer" id="flipContainer">
                                 <img id="songArt" src="${largeArt}">
                                 <div class="song-desc">
-                                    <div class="content">
+                                    <div class="content-no-padding">
                                         <div class="row text-center" id="descNavBar">
                                             <button class="btn-transparent" id="desc-likes"><i class="fa fa-heart small" aria-hidden="true"></i><span>${track.likes_count}</span></button>
                                             <a href="${track.permalink_url + '/comments'}" target="_blank"><button class="btn-transparent" id="desc-comments"><i class="fa fa-comment small" aria-hidden="true"></i><span>${track.comment_count}</span></button></a>
@@ -28,12 +28,12 @@ const SongInfo = (largeArt, track, tags, errorPurchase) => (
                                     </div>
                                 </div>
                             </div>
-                            <h6 class="light">${track.title}</h6>
-                            <p class="title">${track.user.username}</p>
-                            <p class="subtitle" id="curTime">0:00 / 4:20</p>
-                            <div class="waveform"></div>
                         </div>
                     </div>
+                    <h6 class="light">${track.title}</h6>
+                    <p class="title">${track.user.username}</p>
+                    <p class="subtitle" id="curTime">0:00 / 4:20</p>
+                    <div class="waveform"></div>
                 </div>
             </div>
         </div>
