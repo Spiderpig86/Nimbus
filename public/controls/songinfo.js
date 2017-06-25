68e9d65c9cf16d069d2f8905dcbf3c59780f2c71
Stan <slim679975@gmail.com>
2017-06-25 14:36:45 -0400

    Started working on actions dialog

--- public/controls/songinfo.js
+++ public/controls/songinfo.js
@@ -2,7 +2,7 @@ const SongInfo = (largeArt, track, tags, errorPurchase) => (
     `
         <div class="card level">
             <div class="content-no-padding text-center">
-                    <div class="row center">
+                    <div class="row center" id="songArt">
                         <div class="col-12">
                             <div class="flipContainer" id="flipContainer">
                                 <img id="songArt" src="${largeArt}">
