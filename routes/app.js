efd1ecb2208f562f18bd13f4f0cd6166da6f4962
Stan <slim679975@gmail.com>
2017-07-04 23:21:36 -0400

    Removed php express

--- routes/app.js
+++ routes/app.js
@@ -3,28 +3,12 @@ let app = express()
 let path = require('path')
 const port = process.env.PORT || 8000
 
-// must specify options hash even if no options provided!
-let phpExpress = require('php-express')({
- 
-  // assumes php is in your PATH
-  binPath: 'php'
-});
-
 // Set up static file paths
 app.use(express.static(path.join(__dirname, '../public/')));
 app.use('/js', express.static(path.join(__dirname, '../public/js')));
 app.use('/css', express.static(path.join(__dirname, '../public/css')));
 app.use('/fonts', express.static(path.join(__dirname, '../public/fonts')));
 app.use('/php', express.static(path.join(__dirname, '../public/php')));
- 
-// set view engine to php-express
-app.engine('php', phpExpress.engine);
-app.set('view engine', 'php');
- 
-// routing all .php file to php-express
-app.all(/.+\.php$/, phpExpress.router);
-
-
 
 app.get('/', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
